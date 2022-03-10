99fcf7aa5be5210c082dd9c483717e1fb1204f93
Koray Yanik <fumyuun@gmail.com>
2022-03-10 09:16:10 +0000

    Handle additional header fields

--- handle_connection.sh
+++ handle_connection.sh
@@ -1,21 +1,23 @@
 #!/bin/bash
 
+request=""
+path=""
+path_sane=""
+
+request_keys=()
+request_values=()
+
 while read -r line; do
-  echo "[$(date +%T)] $line" >> connection_raw.log
   line=$(echo "$line" | tr -d '\r\n')
 
-  # extract path
-  path=$(echo "$line" | cut -d ' ' -f2)
+  echo "[$(date +%T)] '$line'" >> connection_raw.log
 
-  # remove slashes and dots in the beginning
-  path_sane=$(echo $path | sed 's/^[./]*//')
-
-  if [[ "$line" =~ ^GET ]]; then
-    echo "[$(date +%T)] GET line: $line; path: $path; path_sane: $path_sane" >> connection.log
-
-    if [ -n "$STATIC_DIR" -a -n "$path_sane" -a -r "$STATIC_DIR/$path_sane" ]; then
+  # End of request, time to handle it
+  if [ -z "$line" ]; then
+    # GET, static content directory defined and matches a file
+    if [ "$request" == "GET" -a -n "$STATIC_DIR" -a -n "$path_sane" -a -r "$STATIC_DIR/$path_sane" ]; then
       filetype=$(echo "$path_sane" | sed 's/^.*\.//')
-      content="text/html"
+      content="text/plain"
 
       case "$filetype" in
       css)
@@ -24,26 +26,54 @@ while read -r line; do
       js)
         content="application/javascript"
         ;;
+      html)
+        content="text/html"
+        ;;
       esac
+
+    echo "[$(date +%T)] GET static, path: $path; path_sane: $path_sane" >> connection.log
 
 cat <<EOF
 $($BASHSERV_DIR/header.sh -t $content -l $(wc -c $STATIC_DIR/$path_sane | cut -d ' ' -f1))
 $(cat $STATIC_DIR/$path_sane)
 EOF
-      break
+      exit 0
     fi
 
-    if [ -n "$GET_HANDLER" ]; then
-      $GET_HANDLER "$path_sane" "$path"
-      break
+    # GET, get handler registered
+    if [ "$request" == "GET" -a -n "$GET_HANDLER" ]; then
+      echo "[$(date +%T)] GET dynamic, path: $path; path_sane: $path_sane" >> connection.log
+      request_fields=""
+      for i in $(seq 0 $((${#request_keys[@]} - 1))); do
+        key="${request_keys[$i]}"
+        value="${request_values[$i]}"
+        length=$(echo "$value" | wc -w)
+        request_fields+="$length $key $value "
+      done
+
+      export REQUEST_PATH="$path"
+      export REQUEST_PATH_SANE="$path_sane"
+
+      $GET_HANDLER $request_fields
+      ret=$?
+      [ $ret -eq 0 ] && exit 0
     fi
   fi
-  if [[ "$line" =~ ^BREW && "$path" =~ ^coffee ]]; then
-    body="short and stout\n"
-cat <<EOF
-$($BASHSERV_DIR/header.sh -t "text/plain" -l $(echo -ne "$body" | wc -c) "418 I'm a teapot")
-$(echo -ne $body)
-EOF
+
+  # GET request - GET <path> HTTP/x.x
+  if [[ "$line" =~ ^GET ]]; then
+    request="GET"
+    path=$(echo "$line" | cut -d ' ' -f2)
+    path_sane=$(echo "$path" | sed 's/^[./]*//')
+    continue
+  fi
+
+  # Parse request fields of form Key: Value
+  key=$(echo "$line" | cut -d ':' -f1 | sed 's/^\s*//')
+  val=$(echo "$line" | cut -d ':' -f2-)
+  if [ -n "$key" -a -n "$val" ]; then
+    request_keys+=("$key")
+    request_values+=("$val")
   fi
 done
 
